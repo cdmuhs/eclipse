e00be32ea55a9e99bd54a9281f174268bae54294
cdmuhs <cdmuhs@gmail.com>
2017-10-24 10:16:28 -0700

    Added readme; some code cleanup

---
+++ README.md
@@ -0,0 +1,17 @@
+# Portal eclipse data analysis
+
+This project includes scripts to visualize freeway station data in Portal.
+We wanted to explore how the solar eclipse on 8/21/17 affected traffic patterns, specifically, what speed-flow diagrams looked like.
+
+
+Portal is the regional transportation data archive in the Portland-Vancouver metro region.
+[http://portal.its.pdx.edu](http://portal.its.pdx.edu)
+
+[Speed-flow diagrams](https://www.google.com/search?q=traffic+speed+flow+curves)
+
+Data are analyzed in Wilsonville, close to the I-5 Boone Bridge and I-5 Wilsonville freeway interchange. 
+This is the southern-most point in the metro region available from Portal "stations" data".
+
+# Scripts
+* volume_speed_plots.R - make volume-speed plots for eclipse traffic
+* animate_volume_speed_plot.R - animate the volume-speed plot by hour on the day of the eclipse
--- animate_volume_speed_plot.R
+++ animate_volume_speed_plot.R
@@ -1,6 +1,6 @@
 # C. Muhs
-# 8/21/17
-# try to animate Portal plots
+# 10/24/17
+# Animate Portal speed-flow plots
 
 library(tidyverse)
 library(lubridate)
@@ -11,8 +11,7 @@ library(gganimate)
 
 #############################
 # Load data
-load(file = "C://temp//eclipse_data.Rdata")
-setwd("C://temp//rdir")
+load(file = "data//eclipse_data.Rdata")
 
 #############################
 # Add a field for sequential ordering of time and day plots
@@ -31,9 +30,9 @@ p <- ggplot(data = nb_data, aes(tot_vol, avg_spd, frame = hour_of_day)) +
   geom_point(size = 3, colour = "steelblue4", alpha = 0.9) +
   xlab("Volume, vph") + ylab("Speed, mph")
   
+# p
+# Notice we added frame = hour_of_day and saved the plot as p. 
 
-
-p
-# Notice we added frame = year and saved the plot as p. 
 # We then display it as an animation with the gg_animate function:
+setwd("outputs//")
 gganimate(p, interval = 0.2, "output.gif")
